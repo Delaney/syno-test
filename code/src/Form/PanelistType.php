414e935c7d94053681846aa4331d79de6ce6a461
Delaney Sylvans <delaneysylvans@gmail.com>
2024-06-11 07:10:53 +0100

    add surveys to panelist edit & show pages

--- code/src/Form/PanelistType.php
+++ code/src/Form/PanelistType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Panelist;
+use App\Entity\Survey;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -21,6 +25,17 @@ class PanelistType extends AbstractType
             ->add('receiveNewsletters', CheckboxType::class, [
                 'label' => 'Receive Newsletters',
                 'required' => false,
+            ])
+            ->add('surveys', EntityType::class, [
+                'class' => Survey::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'query_builder' => function (EntityRepository $er): QueryBuilder {
+                    return $er->createQueryBuilder('s')
+                        ->where('s.active = true')
+                        ->orderBy('s.createdAt', 'desc');
+                },
             ])
         ;
     }
